0439328b5cfb329a6ec1b16723495f093a8457a5
Mark Needham <m.h.needham@gmail.com>
2012-02-25 02:33:14 +0000

    mark: attempt at scanl

--- baby.hs
+++ baby.hs
@@ -1,4 +1,4 @@
-data CarParkState = Available Bool Int Int | AlmostFull Bool Int Int | Full Bool Int Int deriving (Show)
+data CarParkState = Available Bool Int Int | AlmostFull Bool Int Int | Full Bool Int deriving (Show)
 data Action = Entering | Leaving deriving (Show) 
 data Sticker = Handicap | None deriving (Show) 
 
@@ -9,35 +9,49 @@ convert instruction
     | instruction == "LH" = (Leaving, Handicap)   
     | otherwise = error "Unrecognised instruction" 
 
-processed_instructions = map convert (process instructions)    
+none :: CarParkState -> (Int -> Int) -> CarParkState
+none (Available _  normal handicap) fn = Available False (fn normal) handicap
+none (AlmostFull _  normal handicap) fn = AlmostFull False (fn normal) handicap
+
+handicap :: CarParkState -> (Int -> Int) -> CarParkState
+handicap (Available _  normal handicap) fn = Available False normal (fn handicap)
+handicap (AlmostFull _  normal handicap) fn = AlmostFull False normal (fn handicap)
 
 car_park_state :: CarParkState -> (Action, Sticker) -> CarParkState
 car_park_state (Available _ 8 handicap) (Entering, None) = AlmostFull True 9 handicap
-car_park_state (AlmostFull _ 11 handicap) (Entering, None) = Full True 12 handicap
+car_park_state (AlmostFull _ 11 handicap) (Entering, None) = Full True handicap
 car_park_state (AlmostFull _ 9 handicap) (Leaving, None) = Available True 8 handicap
-car_park_state (Full _ _ handicap) (Leaving, None) = AlmostFull True 11 handicap
+car_park_state (Full _ handicap) (Leaving, None) = AlmostFull True 11 handicap
+car_park_state (Full _ handicap) (Leaving, Handicap) = Full True handicap
+car_park_state (Full _ _) _ = error "The car park is already full!"
 
-car_park_state (_ _ normal handicap) (Entering, None) = Available False (normal + 1) handicap
-car_park_state (Available _ normal handicap) (Entering, Handicap) = Available False normal (handicap + 1)
-car_park_state (Available _ normal handicap) (Leaving, None) = Available False (normal - 1) handicap
-car_park_state (Available _ normal handicap) (Leaving, Handicap) = Available False normal (handicap - 1)
+car_park_state state (Entering, None) = none state (+ 1)
+car_park_state state (Entering, Handicap) = handicap state (+ 1)
+car_park_state state (Leaving, None) = none state (subtract 1)
+car_park_state state (Leaving, Handicap) = handicap state (subtract 1)
 
-car_park_state (AlmostFull _ normal handicap) (Entering, None) = AlmostFull False (normal + 1) handicap
-car_park_state (AlmostFull _ normal handicap) (Entering, Handicap) = AlmostFull False (normal) (handicap + 1)
-car_park_state (AlmostFull _ normal handicap) (Leaving, None) = AlmostFull False (normal - 1) handicap
-car_park_state (AlmostFull _ normal handicap) (Leaving, Handicap) = AlmostFull False (normal) (handicap - 1)
-
-car_park_state (Full _ _ _) (_, _) = error "The car park is already full!"
-
-changed :: CarParkState -> Bool
-changed (Available True _ _)  = True
-changed (AlmostFull True _ _ )= True
-changed (Full True _ _ )= True
-changed x = False
+sign_changed :: CarParkState -> Bool
+sign_changed (Available True _ _)  = True
+sign_changed (AlmostFull True _ _ )= True
+sign_changed (Full True _ )= True
+sign_changed x = False
 
 instructions = "EN EN EH EN EN EH EN EN LN EN EN LH EN LN LN LN LN"
-process input = words input
 
 entry :: [Char] -> [CarParkState]
 entry instructions = 
-	filter changed (scanl (\acc x -> car_park_state acc x) (Available True 4 2) (map convert (process instructions)))
+	filter sign_changed (scanl (\acc x -> car_park_state acc x) (Available True 4 2) (map convert (words instructions)))
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "no maximum for an empty list"
+maximum' [x] = x
+maximum' (x:xs) 
+	| x > maxTail = x
+	| otherwise = maxTail
+	where maxTail = maximum' xs
+
+
+recscanl :: (a -> b -> a) -> a -> [b] -> [a]
+recscanl fn acc [] = []
+recscanl fn acc (x:xs) = [fn acc x] ++ (recscanl fn (fn acc x) xs)
+
