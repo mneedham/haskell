d01edaea2fc2749e49d4b02169eb6d1fb642b632
Mark Needham <m.h.needham@gmail.com>
2012-03-20 22:45:05 +0000

    mark: inlining the merge function

--- merge_sort.hs
+++ merge_sort.hs
@@ -1,13 +1,12 @@
 msort :: [Int] -> [Int]
 msort unsorted = 	
 	let n = floor (fromIntegral(length unsorted) / 2)
-	in if n == 0 then unsorted 
-		else  
+	in 
+		if n == 0 then unsorted 
+	   	else  
 			let (left, right) = splitAt n unsorted
 			in merge (msort left) (msort right)
-
-merge :: [Int] -> [Int] -> [Int]
-merge [] right = right
-merge left [] = left
-merge left@(x:xs) right@(y:ys) = if x < y then x : merge xs right else y : merge left ys
-		
+	where 
+		merge [] right = right
+		merge left [] = left
+		merge left@(x:xs) right@(y:ys) = if x < y then x : merge xs right else y : merge left ys
