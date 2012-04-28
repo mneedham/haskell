76df858636db36cb3619f22f1e30370cae089a8d
Mark Needham <m.h.needham@gmail.com>
2012-04-28 22:45:31 +0100

    Mark: handle multiple search terms

--- rabinKarp.hs
+++ rabinKarp.hs
@@ -3,17 +3,24 @@ import Data.List
 import Data.Maybe
 import Control.Monad
 import System
+import Data.Set hiding (map)
 
-rabinKarp :: String -> String -> Int
-rabinKarp text pattern = 
-	if pattern == "" 
+rabinKarp :: String -> [String] -> Int
+rabinKarp text patterns = 
+	if length patterns == 0 
 		then -1 
 	else 
 		fromMaybe (-1) $ mapOnMaybe fst $ find matchingString $ zip [0..] $ scanl nextHash (hash text m) $ windowed (m+1) text					
 	where n = length text
-	      m = length pattern	      
+	      m = length $ head patterns      
 	      nextHash currentHash chars = reHash currentHash (head chars) (last chars) m
-	      matchingString (offset, textHash) = hash pattern m == textHash && pattern == subString text offset m 
+	      matchingString (offset, textHash) = contains (\hash -> hash == textHash) patternHashes && contains (\pattern -> pattern == subString text offset m) patterns
+	      patternHashes = map (\pattern -> hash pattern m) patterns
+
+contains :: (a -> Bool) -> [a] -> Bool
+contains fn values = trueOrFalse $ find fn values
+	where trueOrFalse Nothing = False 
+	      trueOrFalse (Just x) = True
 
 mapOnMaybe :: (a -> b) -> Maybe a -> Maybe b
 mapOnMaybe fn (Just x) = Just (fn x)   
@@ -63,10 +70,10 @@ main = do
 		if length args < 2 then putStrLn "Usage: ./rabinKarp pattern file"
 		else
 			do				
-				let (file:pattern:_) = args
-				    m = length pattern				    
+				let (file:patterns) = args
+				    m = length $ head patterns
 				lines <- getLines file
-				putStrLn  $ fromMaybe noMatch . mapOnMaybe (format m) . find aMatch . map (\(idx, line) -> (idx, line, rabinKarp line pattern)) . (zip [1..]) $ lines		
+				putStrLn  $ fromMaybe noMatch . mapOnMaybe (format m) . find aMatch . map (\(idx, line) -> (idx, line, rabinKarp line patterns)) . (zip [1..]) $ lines		
 				where format m (idx, line, match) = flatten [show idx, " ", take match line, greenify (subString line match m), drop (match + m) line]
 				      aMatch (_,_, match) = match /= -1
 				      noMatch = "No match found"
